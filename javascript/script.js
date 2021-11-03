66e826e9988e99b247eb67caafa47ce02ea9321e
Ivy Do <ivy.do2011@gmail.com>
2021-11-02 23:38:57 -0400

    if statement for numbers choice

--- javascript/script.js
+++ javascript/script.js
@@ -4,18 +4,18 @@
 
 function generatePassword() {
 
-  // Array for each condition  
+  // Array for each choice   
   var uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  var lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   var numbArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
   var characterArray = ["@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"];
     
-// Empty array
+  // Empty array to hold all the choices
 
   var resultArray = [];
   var passwordLength
   
-// Asks the user to designate length and confirm/reject the conditions 
+  // Asks the user to designate length and confirm/reject the conditions 
 
   // var passwordLengthPrompt = prompt ("Choose a password length between 8-128 characters.");
   // var uppercase = confirm ("Click 'OK' to include uppercase letters. 'CANCEL' to exclude uppercase letters.");
@@ -23,28 +23,38 @@ function generatePassword() {
   // var numbers = confirm ("Click 'OK' to include numbers. 'CANCEL' to exclude numbers.");
   // var special = confirm ("Click 'OK' to include special characters. 'CANCEL' to special characters.");
 
-  passwordLengthPrompt = prompt ("Choose a password length between 8-128 characters.");
+  passwordLengthPrompt = prompt("Choose a password length between 8-128 characters.");
   if (parseInt(passwordLengthPrompt) > 7 && parseInt(passwordLengthPrompt) < 128) {
     passwordLengthPrompt = parseInt(passwordLengthPrompt);
-    //Logs the password length
-    console.log("The password will have" + passwordLengthPrompt + " characters.");
-   } else {
-    alert("Count again... You MUST choose a length between 8-128 characters.");
+    // Logs the password length
+    console.log("The password will have " + passwordLengthPrompt + " characters.");
+  } else {
+    alert("Count again... You MUST choose a length between 8-128 characters. 🤔");
     generatePassword();
   }
   
-  uppercase = confirm ("Click 'OK' to include uppercase letters. 'CANCEL' to exclude uppercase letters.");
+  // Asks the user if they want to include uppercase.
+  uppercase = confirm("Click 'OK' to include uppercase letters. 'CANCEL' to exclude uppercase letters.");
   if (uppercase) {
     resultArray = resultArray.concat(uppercaseArray);
   }
   
+  // Asks the users if they want to include lowercase letters.
   lowercase = confirm("Click 'OK' to include lowercase letters. 'CANCEL' to exclude lowercase letters.");
   if (lowercase) {
     resultArray = resultArray.concat(lowercaseArray);
   }
 
-  
+  // Asks the user if they want to include number.
+  numbers = confirm("Click 'OK' to include numbers. 'CANCEL' to exclude numbers.");
+  if (numbers) {
+    resultArray = resultArray.concat(numbArray);
+  }
+
+
+
 }
+
 
 var generateBtn = document.querySelector("#generate");
 
